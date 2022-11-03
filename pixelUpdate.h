00984a94f370ae45e808ca1f92f2c51da4096409
Zin <chasehap@gmail.com>
2022-11-02 22:08:20 -0400

    Cole was being mean...

---
+++ pixelUpdate.h
@@ -0,0 +1,15 @@
+//
+// Created by Chase Hap on 11/2/22.
+//
+
+using namespace std;
+
+#ifndef DSAPROJECT3_PIXELUPDATE_H
+#define DSAPROJECT3_PIXELUPDATE_H
+
+class pixelUpdate{
+public:
+    pixelUpdate(){}
+};
+
+#endif //DSAPROJECT3_PIXELUPDATE_H
