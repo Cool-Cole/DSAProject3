654ead593c876b8875475667ff7c65a74efb2a5d
Cole <git@cole.pizza>
2022-11-08 14:18:45 -0500

    Create a header file for the CSV Parsing functions

--- CSVparse.h
+++ CSVparse.h
@@ -5,6 +5,27 @@
 #ifndef DSAPROJECT3_CSVPARSE_H
 #define DSAPROJECT3_CSVPARSE_H
 
+#include <vector>
 
+// This is a function I wrote to help parse strings
+// I have been using it since programming 2
+// This function takes a string and splits it into a vector of strings seperated by a delimiter
+vector<string> stringToVector(string str, char delim){
+    vector<string> ret;
+    string temp;
+
+    for(auto i = 0; i < str.size(); i++){
+        if(str[i] != delim){
+            temp += str[i];
+        } else {
+            ret.push_back(temp);
+            temp.clear();
+        }
+    }
+
+    ret.push_back(temp);
+
+    return ret;
+}
 
 #endif //DSAPROJECT3_CSVPARSE_H
