9f2a18ae671f4d6dc8e86444b6b20159b0639527
Cole <git@cole.pizza>
2022-12-03 13:26:31 -0500

    Set up the basics of a hashmap

--- hashmap.h
+++ hashmap.h
@@ -15,16 +15,26 @@ class Hashmap {
     unsigned int nodeCount;
     unsigned int bucketCount;
 
-    vector<pixelUpdate*>
+    vector<vector<pixelUpdate*>> buckets;
 
 public:
 
     Hashmap(){
         nodeCount = 0;
-        bucketCount = 100;
+        bucketCount = 2000;
+
+        buckets.resize(bucketCount);
     }
 
-    void insert
+    void insert(pixelUpdate* temp){
+        nodeCount++;
+
+        buckets.at(hash(temp->pixelX, temp->pixelY)).push_back(temp);
+    }
+
+    unsigned int hash(unsigned int x, unsigned int y){
+        return x ^ y;
+    }
 
 };
 
