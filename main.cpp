0544a62049b7e304e2b05919a2f33e9253fa6478
Cole <git@cole.pizza>
2022-12-06 20:02:40 -0500

    final final

--- main.cpp
+++ main.cpp
@@ -82,7 +82,7 @@ int main(int argc, char **argv) {
                     if (returnedPixelUpdate == nullptr)
                         cout << "No results found" << endl;
                     else
-                        returnedPixelUpdate->printPixelUpdate("coordinates");
+                        returnedPixelUpdate->printPixelUpdate("userid");
 
                 }
                 else if(userChoice == 2){
